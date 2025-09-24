19a98c7fc61ddaa7db24d6a4cd0e6f3feca34639
Anthony Fu <github@antfu.me>
2025-09-24 09:30:03 +0900

    chore: cleanup

--- build.config.ts
+++ build.config.ts
@@ -17,7 +17,6 @@ export default defineBuildConfig({
   },
   hooks: {
     'rollup:dts:options': (ctx, options) => {
-      // @ts-expect-error - remove commonjs plugin
       options.plugins = options.plugins.filter(i => i?.name !== 'commonjs')
     },
   },
