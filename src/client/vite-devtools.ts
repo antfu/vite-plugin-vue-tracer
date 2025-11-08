82aae4c55d064b8485de37631c634517ced814c2
Anthony Fu <github@antfu.me>
2025-11-08 17:37:46 +0900

    feat: adapt to vite devtools alpha.16

--- src/client/vite-devtools.ts
+++ src/client/vite-devtools.ts
@@ -3,14 +3,21 @@ import type { DockClientScriptContext } from '@vitejs/devtools-kit/client'
 import { events, isEnabled } from 'vite-plugin-vue-tracer/client/listeners'
 import { state } from 'vite-plugin-vue-tracer/client/overlay'
 
-export default function enable(ctx: DockClientScriptContext): void {
-  events.on('click', (e) => {
-    ctx.rpc['vite:core:open-in-editor'](`${e.pos[0]}:${e.pos[1]}:${e.pos[2]}`)
-    state.isVisible = false
+export default function clientScriptSetup(ctx: DockClientScriptContext): void {
+  ctx.current.events.on('entry:activated', () => {
+    events.on('click', (e) => {
+      ctx.rpc['vite:core:open-in-editor'](`${e.pos[0]}:${e.pos[1]}:${e.pos[2]}`)
+      state.isVisible = false
+      state.isEnabled = false
+      ctx.docks.switchEntry(null)
+    })
+
+    isEnabled.value = true
+    state.isVisible = true
   })
 
-  isEnabled.value = !isEnabled.value
-  state.isVisible = isEnabled.value
-
-  ctx.docks.switchEntry(isEnabled.value ? ctx.current.entryMeta.id : null)
+  ctx.current.events.on('entry:deactivated', () => {
+    isEnabled.value = false
+    state.isVisible = false
+  })
 }
